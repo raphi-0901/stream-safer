3229cbcd45ec73c18236159f78bf2ea31ffc0368
Raphael Wirnsberger <rwirnsberger@anexia-it.com>
2024-03-12 14:18:27 +0100

    add option to stop recording

--- index.js
+++ index.js
@@ -8,39 +8,64 @@ dotenv.config()
 
 console.log("Register bot!");
 const telegramBot = new TelegramBot();
-telegramBot.bot.on(/^\/record (.+) (\d+) (.+)$/, (msg, props) => {
+
+const map = new Map();
+
+telegramBot.bot.on(/^\/record (.+)(?: (\d+))? (.+)$/, (msg, props) => {
     const streamUrl = props.match[1];
     const duration = props.match[2];
     const outputFilename = props.match[3];
-
     console.log('streamUrl :>> ', streamUrl, duration, outputFilename);
+
+    if (map.has(streamUrl)) {
+        return telegramBot.sendMessage("Already recording this stream.");
+    }
+
     startRecording(streamUrl, duration, outputFilename, msg.message_id);
     return telegramBot.sendMessage("Recording started.");
 });
 
+telegramBot.bot.on("/stop", (msg) => {
+    if (!msg.reply_to_message?.text) {
+        return telegramBot.sendMessage("Nothing selected to stop.");
+    }
+
+    const parts = msg.reply_to_message.text.match(/^\/record (.+)(?: (\d+))? (.+)$/)
+    if (parts && parts.length > 2 && map.has(parts[1])) {
+        killStreamSafer(map.get(parts[1]))
+
+        return telegramBot.sendMessage("Recording stopped.");
+    }
+
+    return telegramBot.sendMessage("No stream found to stop.");
+});
+
 function startRecording(streamUrl, duration, outputFilename, replyToMessage) {
     const webmOutputFilename = outputFilename + ".webm";
-    const childProcess = spawn('node', ['src/stream-safer.js', streamUrl, duration, webmOutputFilename], { stdio: 'inherit' });
-
+    const videoDuration = isNaN(duration) ? 120 : duration
+    const childProcess = spawn('node', ['src/stream-safer.js', streamUrl, webmOutputFilename], { stdio: 'inherit' });
+    map.set(streamUrl, childProcess.pid);
     const MINUTES_MILLISECONDS = 1000 * 60;
 
-    const timeout = setTimeout(() => {
-        console.log(`Killing child process after ${duration} minutes.`);
-        treeKill(childProcess.pid, "SIGTERM")
-        console.log('childProcess :>> ', childProcess.killed);
-    }, duration * MINUTES_MILLISECONDS);
+    const timeout = setTimeout(() => killStreamSafer(childProcess.pid), videoDuration * MINUTES_MILLISECONDS);
 
     childProcess.on('exit', (code, signal) => {
         console.log(`Child process exited with code ${code} and signal ${signal}.`);
         clearTimeout(timeout);
+        map.delete(streamUrl);
 
         setTimeout(() => {
             // wait a little bit before sending
             telegramBot.sendFile(webmOutputFilename, outputFilename, replyToMessage)
 
-            setTimeout(removeFile(webmOutputFilename), MINUTES_MILLISECONDS);
-        }, 20000)
+            setTimeout(() => removeFile(webmOutputFilename), MINUTES_MILLISECONDS);
+        }, MINUTES_MILLISECONDS * 0.5)
     });
+}
+
+function killStreamSafer(pid) {
+    console.log(`Killing child process after minutes.`);
+    treeKill(pid, "SIGTERM")
 }
 
 function removeFile(filePath) {
--- src/stream-safer.js
+++ src/stream-safer.js
@@ -3,18 +3,16 @@ import ffmpeg from 'fluent-ffmpeg';
 const args = process.argv;
 
 // Checking if the correct number of arguments are provided
-if (args.length !== 5) {
+if (args.length !== 4) {
     console.log("Usage: node node_script.js <string> <number>");
     process.exit(1); // Exit with a non-zero status code to indicate failure
 }
 
 // Extracting input parameters
 const vlcStreamUrl = args[2];
-const duration = parseInt(args[3]);
-const outputFilename = args[4];
+const outputFilename = args[3];
 
 console.log('vlcStreamUrl :>> ', vlcStreamUrl);
-console.log('duration :>> ', duration);
 console.log('outputFilename :>> ', outputFilename);
 
 // Start capturing the VLC stream and save it as an MP4 file
